b1fc0d6075d643d5e889e4b90fd32dded9b48b07
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-12-06 14:09:16 +0600

    Dependency injection without Spring

---
+++ src/main/java/guru/springframework/sfgdi/controller/ConstructorInjectedController.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgdi.controller;
+
+import guru.springframework.sfgdi.services.GreetingService;
+
+public class ConstructorInjectedController {
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/main/java/guru/springframework/sfgdi/controller/PropertyInjectedController.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgdi.controller;
+
+import guru.springframework.sfgdi.services.GreetingService;
+
+public class PropertyInjectedController {
+    public GreetingService greetingService;
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
