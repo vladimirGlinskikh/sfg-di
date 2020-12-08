f1c97f116745787f49d132a753f952d7f91ee252
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-12-08 22:00:40 +0600

    Spring profiles

---
+++ src/main/java/guru/springframework/sfgdi/services/I18nRussianGreetingService.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("RU")
+@Service("i18nService")
+public class I18nRussianGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Привет Spring - RU";
+    }
+}
